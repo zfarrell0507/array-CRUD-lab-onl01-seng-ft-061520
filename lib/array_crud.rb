56b9c1174042b9efd6930d57568bbf38dc670f49
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-24 12:47:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[0]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
